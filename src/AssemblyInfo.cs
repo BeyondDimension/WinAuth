6eb05d0afd3d411aacca7f63bccdc2592925008c
Aigio Liu <lysoft.pw@foxmail.com>
2023-06-02 15:06:37 +0800

    🎨 sync proj structure

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+#pragma warning disable IDE0079 // 请删除不必要的忽略
+#pragma warning disable SYSLIB0025 // 类型或成员已过时
+[assembly: SuppressIldasm]
+#pragma warning restore SYSLIB0025 // 类型或成员已过时
+#pragma warning restore IDE0079 // 请删除不必要的忽略
+#if WINDOWS7_0_OR_GREATER
+[assembly: SupportedOSPlatform("Windows10.0.14393")]
+#endif
--- src/ImplicitUsings.Common.cs
+++ src/ImplicitUsings.Common.cs
@@ -1,20 +1,36 @@
 // C# 10 定义全局 using
 
-#if _IMPORT_COMMON_CONTROLLERS__
-global using BD.Common.Controllers;
-global using BD.Common.Controllers.Abstractions;
+#pragma warning disable IDE0079 // 请删除不必要的忽略
+#pragma warning disable IDE0005
+#pragma warning disable SA1209 // Using alias directives should be placed after other using directives
+#pragma warning disable SA1211 // Using alias directives should be ordered alphabetically by alias name
+
+#if !APP_HOST
+#if !NETFRAMEWORK
+
+#if !__NOT_IMPORT_COMMON_PRIMITIVES__
+global using BD.Common;
+global using BD.Common.Columns;
+global using BD.Common.Enums;
 #endif
+
 #if _IMPORT_COMMON_IDENTITY__
 global using BD.Common.Identity;
 global using BD.Common.Identity.Abstractions;
 #endif
-#if _IMPORT_COMMON_SERVICES__
+
+#if !__NOT_IMPORT_COMMON_SERVICES__
 global using BD.Common.Services;
+global using BD.Common.Services.Implementation;
 #endif
-#if USE_SMS
-global using BD.Common.Models.SmsSender;
-global using BD.Common.Services.Implementation.SmsSender;
-#endif
+
 #if _IMPORT_COMMON_MIDDLEWARE__
 global using BD.Common.Middleware;
+#endif
+
+#if !__NOT_IMPORT_COMMON_PRIMITIVES__ && ANDROID
+global using Toast = BD.Common.Toast;
+#endif
+
+#endif
 #endif
